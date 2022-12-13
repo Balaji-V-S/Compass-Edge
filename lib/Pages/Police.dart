e5a3d6df00ac5065d555b5658874c7f92b9a2f4e
Balaji V S <*vsbrobot@gmail.com*>
2022-12-13 20:36:17 +0530

    siren

---
+++ lib/Pages/Police.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class blinker extends StatelessWidget {
+  const blinker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('HI'),
+    );
+  }
+}
