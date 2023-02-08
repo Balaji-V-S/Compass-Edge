df292b4f39611c67b5f7327e165badc07bd05afe
Balaji V S <*vsbrobot@gmail.com*>
2023-02-08 17:44:54 +0530

    Query all package removed
    Query all package removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Compass_Edge/main.dart';
+import 'package:compass_edge/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
